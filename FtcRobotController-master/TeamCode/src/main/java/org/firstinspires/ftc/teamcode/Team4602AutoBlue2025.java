e9f2332f7e68a871b304af891464fc457e65839f
Nobody <nobodyhere618@gmail.com>
2024-09-26 17:51:26 -0500

    Added team code

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4602AutoBlue2025.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (name="Team4602Auto1ParkingBlue", group="4602")
+public class Team4602AutoBlue2025 extends LinearOpMode{
+
+    Team6976HM2025 robot = new Team6976HM2025();
+    ElapsedTime Time = new ElapsedTime();
+    double multy = 0.3;
+
+    @Override
+    public void runOpMode() {
+        robot.Map(hardwareMap);
+        waitForStart();
+
+        //write code here
+
+
+    }
+
+}
