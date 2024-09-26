c0e9e5ef3f888485f9adc714beb1c3b85745b5d7
Nobody <nobodyhere618@gmail.com>
2024-09-26 18:13:28 -0500

    re adding sdk to see if It fixes anything
    help, trust the process. I have no idea what im doing.

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4008AutoRed2025.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (name="Team4008Auto1ParkingRed", group="4008")
+public class Team4008AutoRed2025 extends LinearOpMode{
+
+    Team4008HM2025 robot = new Team4008HM2025();
+    ElapsedTime Time = new ElapsedTime();
+    double multy = 0.3;
+
+    @Override
+    public void runOpMode() {
+        robot.Map(hardwareMap);
+        waitForStart();
+
+        //write code here
+
+
+    }
+
+}
