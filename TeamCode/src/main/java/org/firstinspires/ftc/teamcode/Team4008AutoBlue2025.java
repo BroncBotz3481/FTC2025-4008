98ee1fa34415d15aacedaae847c5562f2f4412a2
fml04 <family6666520@gmail.com>
2024-11-16 16:10:55 -0600

    Add methods for robot moving in four directions &codes for parking in observation zone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4008AutoBlue2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4008AutoBlue2025.java
@@ -2,24 +2,75 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous (name="Team4008Auto1ParkingBlue", group="4008")
-public class Team4008AutoBlue2025 extends LinearOpMode{
+@Autonomous (name="Team4008AutoBlue2025", group="4008")
+public class Team4008AutoBlue2025 extends LinearOpMode {
 
     Team4008HM2025 robot = new Team4008HM2025();
     ElapsedTime Time = new ElapsedTime();
-    double multy = 0.3;
+
 
     @Override
     public void runOpMode() {
         robot.Map(hardwareMap);
+        robot.Servo.setPosition(0);
         waitForStart();
 
-        //write code here
-
+        moveForward(0.4 , 4000);
+    /*
+    start as the alliance on the right & face right
+    Moving distance is about half the field
+    */
 
     }
+
+    public void moveForward (double power, int time){
+        robot.DriveRightFront.setPower(-power);
+        robot.DriveLeftFront.setPower(-power);
+        robot.DriveRightBack.setPower(-power);
+        robot.DriveLeftBack.setPower(-power);
+        sleep(time);
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+    }
+    public void moveBackward (double power, int time){
+        robot.DriveRightFront.setPower(power);
+        robot.DriveLeftFront.setPower(power);
+        robot.DriveRightBack.setPower(power);
+        robot.DriveLeftBack.setPower(power);
+        sleep(time);
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+    }
+    public void moveRight (double power, int time){
+        robot.DriveRightFront.setPower(power);
+        robot.DriveLeftFront.setPower(-power);
+        robot.DriveRightBack.setPower(-power );
+        robot.DriveLeftBack.setPower(power);
+        sleep(time);
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+    }
+
+    public void moveLeft (double power, int time){
+        robot.DriveRightFront.setPower(-power);
+        robot.DriveLeftFront.setPower(power);
+        robot.DriveRightBack.setPower(power );
+        robot.DriveLeftBack.setPower(-power);
+        sleep(time);
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+    }
+
+
 
 }
